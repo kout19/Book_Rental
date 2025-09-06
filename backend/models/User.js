a590d7fedbb2d1ce4e4e804c4411ce71f8006f69
Kefyalew <koutlook19@gmail.com>
2025-09-06 06:51:38 -0900

    settingup backend

---
+++ backend/models/User.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+const userSchema = new mongoose.Schema({
+    firebaseUID: {
+         type: String, 
+         required: true, 
+         unique: true 
+        },
+    name: {
+         type: String, 
+         required: true,
+         Trim: true 
+        },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        lowercase: true,
+        Trim: true
+     },
+    role: { 
+        type: String,
+         enum: ['user', 'admin','owner'], 
+         default: 'user' 
+        },
+    phone:{
+        type: String,
+        required: false,
+        Trim: true
+    },
+    address: {
+        type: String,
+        required: false,
+        Trim: true
+    },
+    ownedBooks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book'
+    }],
+    rentedBooks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book'
+    }], 
+    createdAt: { 
+        type: Date, 
+        default: Date.now 
+    },
+},
+{timestamps: true}
+);
+
+const User = mongoose.model('User', userSchema);
+export default User;
