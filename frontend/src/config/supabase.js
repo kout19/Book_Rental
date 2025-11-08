32f37e5fe5571e4b7a5ee983836ace7aed26994c
Kefyalew <koutlook19@gmail.com>
2025-11-08 10:50:44 +0300

    try to fix supabase url

--- frontend/src/config/supabase.js
+++ frontend/src/config/supabase.js
@@ -1,29 +1,29 @@
-// import { createClient } from "@supabase/supabase-js";
-// import Cookies from "js-cookie";
-// const supabaseUrl= import.meta.env.VITE_SUPABASE_URL;
-// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-// // Public anon key (safe for frontend)
-// const supabase = createClient(supabaseUrl, supabaseAnonKey,{
-//   auth: {
-//        persistSession: false, // we manage it ourselves
-//     storage: {
-//       getItem: (key) => Cookies.getItem(key),
-//       setItem: (key, value) => Cookies.setItem(key, value),
-//       removeItem: (key) => Cookies.removeItem(key),
-//   },
-// }
-// });
-// const token= Cookies.get('token');
-// if (token) {
-//   supabase.auth.setSession({ access_token: token, refresh_token: token });
-// }
-
-// export default supabase;
-
 import { createClient } from "@supabase/supabase-js";
-const supabase = createClient(
- import.meta.env.VITE_SUPABASE_URL,
- import.meta.env.VITE_SUPABASE_ANON_KEY, // use service_role key
-);
+import Cookies from "js-cookie";
+const supabaseUrl= import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Public anon key (safe for frontend)
+const supabase = createClient(supabaseUrl, supabaseAnonKey,{
+  auth: {
+       persistSession: false, // we manage it ourselves
+    storage: {
+      getItem: (key) => Cookies.getItem(key),
+      setItem: (key, value) => Cookies.setItem(key, value),
+      removeItem: (key) => Cookies.removeItem(key),
+  },
+}
+});
+const token= Cookies.get('token');
+if (token) {
+  supabase.auth.setSession({ access_token: token, refresh_token: token });
+}
 
 export default supabase;
+
+// import { createClient } from "@supabase/supabase-js";
+// const supabase = createClient(
+//  import.meta.env.VITE_SUPABASE_URL,
+//  import.meta.env.VITE_SUPABASE_ANON_KEY, // use service_role key
+// );
+
+// export default supabase;
